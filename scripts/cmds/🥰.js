50dce754d54ff638ee0bc8d08c21079a068c4d53
Habibmolla08 <habibmolla0408@gmail.com>
2024-10-31 22:04:15 +0530

    Update 🥰.js

--- scripts/cmds/🥰.js
+++ scripts/cmds/🥰.js
@@ -15,8 +15,8 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "🥰") {
  return message.reply({
  body: " 「 🥰❥︎----ღ᭄_কাউকে ভালবাসলে❞࿐.❤️..\n❥︎----ღ᭄_গাছের শিকড়ের মতো ভালোবাসা উচিত❞࿐.🌴🤎🙂.\n❥︎----ღ᭄_ফুলের মতো নয়❞࿐.🌹....\n❥︎---কারণ.༎༅..🤷‍♂️🍀.\n❥︎----ღ᭄_ফুল তো শুধু সুবাস দিয়েই ঝরে যায়❞࿐.🙂\n❥︎----ღ᭄_আর শিকড় সে তো মৃত্যু আগে পর্যন্ত থেকে যায় ❞࿐🥰🫠\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\nARIF~KHAN」",
- attachment: await global.utils.getStreamFromURL("https://cdn.fbsbx.com/v/t59.3654-21/465055555_1097032058710637_3834832479653525388_n.mp4/audioclip-1730325471000-6339.mp4?_nc_cat=104&ccb=1-7&_nc_sid=d61c36&_nc_eui2=AeHvJgUH_7lNqkqWRGE45ur8AIFVJPLXIQwAgVUk8tchDPbTgrNAQ6SvxVbehU-fIf4ZUQ4R9xNOTOsRMJAC3xsd&_nc_ohc=dOKqXMYM46oQ7kNvgHHszdX&_nc_zt=28&_nc_ht=cdn.fbsbx.com&_nc_gid=A6XDlrF4DR1f8Yz93W_0Swr&oh=03_Q7cD1QEv7kAkleTfuzWGBXzim3bI-UNrcUzzV8A8iecN5TRHRw&oe=6724750D&dl=1")
+ attachment: await global.utils.getStreamFromURL("http://g-v1.onrender.com/RPnMtTtRs.mp4")
  });
  }
  }
- }
+}
