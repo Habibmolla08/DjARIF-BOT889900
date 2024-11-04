7624a5aaaebdac941642d6791a014868a81e89d2
Habibmolla08 <habibmolla0408@gmail.com>
2024-11-04 12:40:11 +0530

    Update 😘.js

--- scripts/cmds/😘.js
+++ scripts/cmds/😘.js
@@ -15,7 +15,7 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "😘") {
  return message.reply({
  body: " 「❥︎----ღ᭄_আই লাভ য়উ আরিফ❞࿐.🙂❥︎---\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\ARIF~KHAN」",
- attachment: await global.utils.getStreamFromURL("https://cdn.fbsbx.com/v/t59.3654-21/465019634_1087957656075690_2877642317146212449_n.mpga/3CfNi9HrPW.mpga?_nc_cat=110&ccb=1-7&_nc_sid=d61c36&_nc_eui2=AeHPASz7RyP7Y9EsI2ADCM2zMeweGzR2efAx7B4bNHZ58CWpyL-AKe4SoNACF5wVS-MLhsS4XzyJWoxGQOiKxdbo&_nc_ohc=sVbEOmtTjX4Q7kNvgHvjfxb&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=AV-CbxypSDneR4YEq0R73Xb&oh=03_Q7cD1QFC7LvpnpLEFRO8q2nfQl487N9f61tyqlZZoL3MOC_t1A&oe=67286707&dl=1")
+ attachment: await global.utils.getStreamFromURL("https://cdn.fbsbx.com/v/t59.3654-21/465019634_1087957656075690_2877642317146212449_n.mpga/7fucuqWdSI.mpga?_nc_cat=110&ccb=1-7&_nc_sid=d61c36&_nc_ohc=gorgRaRlu0sQ7kNvgEdMze8&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=AxELjOawWzfkcRYsdLpcKYY&oh=03_Q7cD1QGvEMxotrgTfyJ8vkGm6ctqDpdV8wVZfPXDnuh8WJWevw&oe=672A45F1&dl=1")
  });
  }
  }
