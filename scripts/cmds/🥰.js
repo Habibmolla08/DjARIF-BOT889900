46f418ce5238f82e223db18b2ef6c5f7641e57c0
Habibmolla08 <habibmolla0408@gmail.com>
2024-11-07 21:06:03 +0530

    Update 🥰.js

--- scripts/cmds/🥰.js
+++ scripts/cmds/🥰.js
@@ -15,7 +15,7 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "🥰") {
  return message.reply({
  body: " 「 🥰❥︎----ღ᭄_কাউকে ভালবাসলে❞࿐.❤️..\n❥︎----ღ᭄_গাছের শিকড়ের মতো ভালোবাসা উচিত❞࿐.🌴🤎🙂.\n❥︎----ღ᭄_ফুলের মতো নয়❞࿐.🌹....\n❥︎---কারণ.༎༅..🤷‍♂️🍀.\n❥︎----ღ᭄_ফুল তো শুধু সুবাস দিয়েই ঝরে যায়❞࿐.🙂\n❥︎----ღ᭄_আর শিকড় সে তো মৃত্যু আগে পর্যন্ত থেকে যায় ❞࿐🥰🫠\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\nARIF~KHAN」",
- attachment: await global.utils.getStreamFromURL("https://cdn.fbsbx.com/v/t59.3654-21/465198246_1068854197770847_6117100358872927033_n.mpga/lLKWZGGij8.mpga?_nc_cat=106&ccb=1-7&_nc_sid=d61c36&_nc_ohc=GWbSw4qwXYAQ7kNvgHiEBlm&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=AV-Fc-YHJHVVrY9hHhCetZg&oh=03_Q7cD1QHOa9FjAgEswh6q2MSHU1QOQUVXjalavmDHIDWrJXR6ww&oe=672A60CB&dl=1")
+ attachment: await global.utils.getStreamFromURL("https://cdn.fbsbx.com/v/t59.3654-21/465198246_1068854197770847_6117100358872927033_n.mpga/lLKWZGGij8.mpga?_nc_cat=106&ccb=1-7&_nc_sid=d61c36&_nc_ohc=5-n3xFg6GL8Q7kNvgEqeSaU&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=Ak3wUKz8IByp-BG-ad5FTFf&oh=03_Q7cD1QGtfAiMKzLoJ4r-NuaLV5wQajKlQABZSSiUGuc6bUN88Q&oe=672EC5CB&dl=1")
  });
  }
  }
