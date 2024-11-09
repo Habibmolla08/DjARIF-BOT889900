baf8a40e4c5f8f1358d5b588fc0ad363d375f4f2
Habibmolla08 <habibmolla0408@gmail.com>
2024-11-09 11:19:37 +0530

    Update 💔.js

--- scripts/cmds/💔.js
+++ scripts/cmds/💔.js
@@ -14,8 +14,8 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "💔") {
  return message.reply({
- body: " 「 「 🥰❥︎----ღ᭄_আশিক বস❞࿐.❤../n❥︎----ღ᭄_ ছেঁকা খাইছে❞࿐.🌴🤎🙂./n❥︎----ღ᭄_কান্না করতাছে❞࿐.🌹..../n❥︎---জা বলার.༎༅..🤷‍♂🍀./n❥︎----ღ᭄_আমারে বলতে পারেন❞࿐.🙂\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\nBOSS~ASHIK」",
- attachment: await global.utils.getStreamFromURL("https://cdn.fbsbx.com/v/t59.3654-21/465198246_1068854197770847_6117100358872927033_n.mpga/lLKWZGGij8.mpga?_nc_cat=106&ccb=1-7&_nc_sid=d61c36&_nc_ohc=5-n3xFg6GL8Q7kNvgEqeSaU&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=AzKItSLcBFk4fEV3gK7OYN_&oh=03_Q7cD1QEuNDEXdg6wfBXryB15uTkkgFI7R6eldjc2VNq23Yj8ag&oe=672EC5CB&dl=1")
+ body: " 「 「 🥰❥︎----ღ᭄_আৱিফ বস❞࿐.❤../n❥︎----ღ᭄_ ছেঁকা খাইছে❞࿐.🌴🤎🙂./n❥︎----ღ᭄_কান্না করতাছে❞࿐.🌹..../n❥︎---জা বলার.༎༅..🤷‍♂🍀./n❥︎----ღ᭄_আমারে বলতে পারেন❞࿐.🙂\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\nBOSS~ARIF」",
+ attachment: await global.utils.getStreamFromURL("https://cdn.fbsbx.com/v/t59.3654-21/465198246_1068854197770847_6117100358872927033_n.mpga/VIFRTdJJf6.mpga?_nc_cat=106&ccb=1-7&_nc_sid=d61c36&_nc_ohc=l8GfSNqgLvUQ7kNvgG8Rc9s&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=A_eMfeEx9bCKyJGdMQb5RV5&oh=03_Q7cD1QGnrNNB0lFpQS0bXI-SeemHw_e_wEttVDoWY05tK6rPPw&oe=6730BA3E&dl=1")
  });
  }
  }
